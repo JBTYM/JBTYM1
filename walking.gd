ada1f67abc729bf610a1c6edeac3eef96a0b6841
snakebunker <char.eliasson@gmail.com>
2024-12-01 20:48:15 +0100

    add spider animations file (also a mesh file that doesn't feature lights camera action), add animation scene, add new animation and mesh files to player scene, change 'walk' to 'walking' in walking script

--- walking.gd
+++ walking.gd
@@ -1,7 +1,7 @@
 extends PlayerState
 
 func enter(previous_state_path: String, data := {}) -> void:
-	player.animation_player.play("walk")
+	player.animation_player.play("walking")
 
 func physics_update(delta: float) -> void:
 	# var input_direction_x := Input.get_axis("turn_left", "turn_right")
